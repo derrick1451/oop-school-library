1e6179a2bc317e1d9a85c233646dc6b8fc084c92
Derrick1451 <haylederrick14@gmail.com>
2023-08-11 13:58:18 +0300

    Update trailing lines- linters

--- rental.rb
+++ rental.rb
@@ -1,7 +1,7 @@
 class Rental
   attr_accessor :date
   attr_reader :person, :book
-  
+
   def initialize(person, book, date)
     @person = person
     person.rentals << self
--- student.rb
+++ student.rb
@@ -1,7 +1,7 @@
 require './person'
 class Student < Person
   attr_reader :classroom
-  
+
   def initialize(age, name = 'Unknown', parent_permission: true)
     super(age, name, parent_permission: parent_permission)
   end
